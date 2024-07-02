7cb37dfffecbebf3afb3318880a8be3eac0821f3
ruleva <139155207+ruleva@users.noreply.github.com>
2024-07-02 20:04:49 +0200

    Added initial code

--- GeekRunnerApp/GeekRunnerApp/Form1.Designer.cs
+++ GeekRunnerApp/GeekRunnerApp/Form1.Designer.cs
@@ -29,12 +29,79 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.background = new System.Windows.Forms.PictureBox();
+            this.pb_character = new System.Windows.Forms.PictureBox();
+            this.lblLevel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblScore = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.background)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_character)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // background
+            // 
+            this.background.Location = new System.Drawing.Point(0, 0);
+            this.background.Name = "background";
+            this.background.Size = new System.Drawing.Size(808, 448);
+            this.background.TabIndex = 0;
+            this.background.TabStop = false;
+            // 
+            // pb_character
+            // 
+            this.pb_character.Location = new System.Drawing.Point(67, 291);
+            this.pb_character.Name = "pb_character";
+            this.pb_character.Size = new System.Drawing.Size(95, 101);
+            this.pb_character.TabIndex = 2;
+            this.pb_character.TabStop = false;
+            // 
+            // lblLevel
+            // 
+            this.lblLevel.AutoSize = true;
+            this.lblLevel.Location = new System.Drawing.Point(28, 31);
+            this.lblLevel.Name = "lblLevel";
+            this.lblLevel.Size = new System.Drawing.Size(44, 16);
+            this.lblLevel.TabIndex = 3;
+            this.lblLevel.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(701, 31);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(44, 16);
+            this.lblScore.TabIndex = 4;
+            this.lblScore.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblLevel);
+            this.Controls.Add(this.pb_character);
+            this.Controls.Add(this.background);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.background)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_character)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox background;
+        private System.Windows.Forms.PictureBox pb_character;
+        private System.Windows.Forms.Label lblLevel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblScore;
     }
 }
 
--- GeekRunnerApp/GeekRunnerApp/Form1.cs
+++ GeekRunnerApp/GeekRunnerApp/Form1.cs
@@ -12,9 +12,81 @@ namespace GeekRunnerApp
 {
     public partial class Form1 : Form
     {
+        private int score;
+        private int highScore;
+        private int gameSpeed;
+        private bool jumping = false;
+        private int jumpSpeed;
+        private int force;
+        private Random random;
+
+        private int level;
+        private readonly int[] levelSpeeds = { 10, 15, 20 };
+        private readonly int[] levelScores = { 10, 20, 30 };
+
         public Form1()
         {
             InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            score = 0;
+            highScore = 0;
+            gameSpeed = levelSpeeds[0];
+            jumping = false;
+            level = 1;
+            jumpSpeed = 0;
+            force = 0;
+            random = new Random();
+            lblLevel.Text = "Level 1";
+            lblScore.Text = "Score: 0";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pb_character.Top += jumpSpeed; //karakterot go pomestuvame spored jumpspeedot, 
+
+            if(jumping && force < 0) //proveruvame dali karakterot treba da prekine da skoka
+            {
+                jumping = false; 
+            }
+            if(jumping) // ako skoka namaluvame vrednost za da odi nagore, vo sprotivno dodeluvame pozitivna za da ide nadole
+            {
+                jumpSpeed = -12;
+                force -= 1;
+            }
+            else
+            {
+                jumpSpeed = 12;
+            }
+            foreach(Control x in this.Controls)
+            {
+                if (x is PictureBox && x.Tag =="obstacle")
+                {
+                    x.Left -= gameSpeed;
+                    if(x.Left < -100)
+                    {
+                        x.Left = this.ClientSize.Width + random.Next(200, 800);
+                        score++;
+                        //levelup funkcija tuka
+                    }
+                    if(pb_character.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        //game over funkcija tuka
+                    }
+
+                }
+            }
+            lblScore.Text = "Score: " + score;
+
         }
     }
 }
